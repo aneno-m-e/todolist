7dae6849cb0909e034cbeae66d68a6325184ffbb
Noemie <wandering_anemone@protonmail.com>
2021-02-18 18:08:09 +1300

    Display todos using Todos.vue component only

---
+++ src/components/Todo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="todo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="todolist">
+    <h2>Cool things to do</h2>
+    <ul>
+      <li v-bind:key="todo.id" v-for='todo in todos'>
+        <span>
+          {{todo.id}} : {{todo.task}}
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Todos',
-  props: {
-  }
+  props: [
+    'todos'
+  ]
 }
 </script>
 
