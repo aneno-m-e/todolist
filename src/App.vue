aaefbe1648a686942f7231163f085c3a3e371d66
Noemie <wandering_anemone@protonmail.com>
2021-02-18 17:47:23 +1300

    Add dummy data

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      todos: [
+        {id:1, task: 'Enjoy the sun'},
+        {id:2, task: 'Defeat cabbage whites'},
+        {id:3, task: 'Go orca-spotting'}
+      ]
+    }
+  },
   components: {
     HelloWorld
   }
